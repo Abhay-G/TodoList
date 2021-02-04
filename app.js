55bc511360e3cb3667c473dec35434fceaf2ef3d
Abhay Gupta <abhaygupta15153986@gmail.com>
2021-02-04 17:12:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname +"/date.js");
+
+const app = express();
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+const items = [];
+const workItems = [];
+
+app.use(express.static("public"));
+app.get("/", function (req,res)
+{
+  const day = date.getDate();
+   res.render('lists',{
+     listTitle:day,
+     addItems:items
+   });
+})
+
+app.get("/work",function(req,res)
+{
+  res.render('lists',{
+    listTitle:"Work List",
+    addItems:workItems
+  })
+})
+
+app.post("/", function (req,res)
+{
+
+  console.log(req.body);
+  if(req.body.chkindex)
+  {
+    let pos = Number(req.body.chkindex);
+    console.log(pos +" --> "+items);
+    items.splice(pos, 1);
+    res.redirect("/");
+  }
+  else{
+  let newItem = req.body.newitem;
+  if(req.body.listButton==="Work")
+  {
+    workItems.push(newItem);
+    res.redirect("/work");
+  }
+  else{
+  items.push(newItem);
+  res.redirect("/");
+}
+}
+})
+app.listen(3000,function(req,res)
+{
+  console.log("Server is running on port 3000");
+})
---
+++ public/scripts/index.js
@@ -0,0 +1,69 @@
+//submiting on checkbox
+$('#chkform .item input[type="checkbox"]').change(function() {
+  $('#chkform').submit();
+});
+
+//cpyright animation
+var copyright = document.querySelector(".cpyright");
+copyright.addEventListener("mouseenter", function() {
+  copyright.innerText = "© Abhay";
+})
+copyright.addEventListener("mouseleave", function() {
+  copyright.innerHTML = "© EGGZY";
+})
+
+//hover animation
+document.querySelector('.addbtn').addEventListener("mouseenter", function() {
+  var t = gsap.timeline();
+  t.to('.addbtn', {
+    duration: 0.3,
+    scale: 1.2,
+    ease: 'Expo.easeInOut',
+
+  })
+})
+document.querySelector('.addbtn').addEventListener("mouseleave", function() {
+  var t = gsap.timeline();
+  t.to('.addbtn', {
+    duration: 0.3,
+    scale: 1,
+    ease: 'Expo.easeInOut',
+
+  })
+
+})
+
+//all other gsap animation
+var t1 = gsap.timeline();
+
+t1.from('#heading', {
+  duration: 1.5,
+  y: 40,
+  ease: 'Expo.easeInOut',
+  opacity: 0
+})
+t1.from('.rest', {
+  duration: 1,
+  y: 60,
+  ease: 'Expo.easeInOut',
+  opacity: 0
+}, '-=1')
+t1.from('button', {
+  duration: 0.8,
+  scale: 5,
+  ease: 'Expo.easeInOut',
+  opacity: 0
+}, '-=0.5')
+t1.from('.footer', {
+  duration: 2,
+  opacity: 0
+}, '-=0.5')
+
+
+$('#heading h1').textillate({
+  initialDelay: 0.5,
+  in: {
+    effect: "fadeIn",
+    delayScale: 2
+  }
+});
